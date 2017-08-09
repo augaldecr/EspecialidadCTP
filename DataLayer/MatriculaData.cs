4f6928360b293b7e286d837f7f9f544184f66ea9
Alonso Ugalde <augaldecr@gmail.com>
2017-08-09 12:48:52 -0600

    Add Bussines Layer

---
+++ DataLayer/MatriculaData.cs
@@ -0,0 +1,125 @@
+﻿using Entities;
+using MySql.Data.MySqlClient;
+using System;
+using System.Configuration;
+using System.Data;
+
+namespace DataLayer
+{
+    public class MatriculaData : Matricula
+    {
+        #region GuardarMatricula
+        public void GuardaMatricula()
+        {
+            string connString = ConfigurationManager.AppSettings["connString"];
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "InsertMatricula";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("estudiante", MySqlDbType.Int16);
+                        cmd.Parameters["estudiante"].Value = Estudiante;
+                        cmd.Parameters["estudiante"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("curso_lectivo", MySqlDbType.Int16);
+                        cmd.Parameters["curso_lectivo"].Value = CursoLectivo;
+                        cmd.Parameters["curso_lectivo"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("especialidad1", MySqlDbType.Int16);
+                        cmd.Parameters["especialidad1"].Value = Especialidad1;
+                        cmd.Parameters["especialidad1"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("especialidad2", MySqlDbType.Int16);
+                        cmd.Parameters["especialidad2"].Value = Especialidad2;
+                        cmd.Parameters["especialidad2"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("especialidad3", MySqlDbType.Int16);
+                        cmd.Parameters["especialidad3"].Value = Especialidad3;
+                        cmd.Parameters["especialidad3"].Direction = ParameterDirection.Input;
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+
+        #region ActualizarEstudiante
+        public void ActualizaEstudiante()
+        {
+            string connString = ConfigurationManager.AppSettings["connString"];
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "UpdateMatricula";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("pidmatricula_admision", MySqlDbType.Int16);
+                        cmd.Parameters["pidmatricula_admision"].Value = IdMatricula;
+                        cmd.Parameters["pidmatricula_admision"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pestudiante", MySqlDbType.Int16);
+                        cmd.Parameters["pestudiante"].Value = Estudiante;
+                        cmd.Parameters["pestudiante"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pcurso_lectivo", MySqlDbType.Int16);
+                        cmd.Parameters["pcurso_lectivo"].Value = CursoLectivo;
+                        cmd.Parameters["pcurso_lectivo"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pespecialidad1", MySqlDbType.Int16);
+                        cmd.Parameters["pespecialidad1"].Value = Especialidad1;
+                        cmd.Parameters["pespecialidad1"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pespecialidad2", MySqlDbType.Int16);
+                        cmd.Parameters["pespecialidad2"].Value = Especialidad2;
+                        cmd.Parameters["pespecialidad2"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pespecialidad3", MySqlDbType.Int16);
+                        cmd.Parameters["pespecialidad3"].Value = Especialidad3;
+                        cmd.Parameters["pespecialidad3"].Direction = ParameterDirection.Input;
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+
+        #region BorrarMatricula
+        public void BorraMatricula()
+        {
+            string connString = ConfigurationManager.AppSettings["connString"];
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "DeleteMatricula";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("idmatricula_admision", MySqlDbType.Int16);
+                        cmd.Parameters["idmatricula_admision"].Value = IdMatricula;
+                        cmd.Parameters["idmatricula_admision"].Direction = ParameterDirection.Input;
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
---
+++ DataLayer/NotaData.cs
@@ -0,0 +1,129 @@
+﻿using Entities;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer
+{
+    public class NotaData : Nota
+    {
+        #region GuardarNota
+        public void GuardaNota()
+        {
+            string connString = ConfigurationManager.AppSettings["connString"];
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "InsertNota";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("estudiante", MySqlDbType.Int16);
+                        cmd.Parameters["estudiante"].Value = Matricula;
+                        cmd.Parameters["estudiante"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("asignatura", MySqlDbType.Int16);
+                        cmd.Parameters["asignatura"].Value = Asignatura;
+                        cmd.Parameters["asignatura"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("nivel", MySqlDbType.Int16);
+                        cmd.Parameters["nivel"].Value = Nivel;
+                        cmd.Parameters["nivel"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("periodo", MySqlDbType.Int16);
+                        cmd.Parameters["periodo"].Value = Periodo;
+                        cmd.Parameters["periodo"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("nota", MySqlDbType.Decimal);
+                        cmd.Parameters["nota"].Value = Calificacion;
+                        cmd.Parameters["nota"].Direction = ParameterDirection.Input;
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+
+        #region ActualizarNota
+        public void ActualizaNota()
+        {
+            string connString = ConfigurationManager.AppSettings["connString"];
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "UpdateNota";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("pidnota", MySqlDbType.Int16);
+                        cmd.Parameters["pidnota"].Value = IdNota;
+                        cmd.Parameters["pidnota"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pestudiante", MySqlDbType.Int16);
+                        cmd.Parameters["pestudiante"].Value = Matricula;
+                        cmd.Parameters["pestudiante"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pasignatura", MySqlDbType.Int16);
+                        cmd.Parameters["pasignatura"].Value = Asignatura;
+                        cmd.Parameters["pasignatura"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pnivel", MySqlDbType.Int16);
+                        cmd.Parameters["pnivel"].Value = Nivel;
+                        cmd.Parameters["pnivel"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pperiodo", MySqlDbType.Int16);
+                        cmd.Parameters["pperiodo"].Value = Periodo;
+                        cmd.Parameters["pperiodo"].Direction = ParameterDirection.Input;
+                        cmd.Parameters.Add("pnota", MySqlDbType.Decimal);
+                        cmd.Parameters["pnota"].Value = Calificacion;
+                        cmd.Parameters["pnota"].Direction = ParameterDirection.Input;
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+
+        #region BorrarNota
+        public void BorraNota()
+        {
+            string connString = ConfigurationManager.AppSettings["connString"];
+
+            try
+            {
+                using (MySqlConnection conn = new MySqlConnection(connString))
+                {
+                    using (MySqlCommand cmd = new MySqlCommand())
+                    {
+                        cmd.Connection = conn;
+                        cmd.CommandText = "DeleteNota";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Parameters.Add("pidnota", MySqlDbType.Int16);
+                        cmd.Parameters["pidnota"].Value = IdNota;
+                        cmd.Parameters["pidnota"].Direction = ParameterDirection.Input;
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
+            catch (System.Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+        #endregion
+    }
+}
