870ab13fb8c015bb845a718167f296211b98e24b
Alonso Ugalde <augaldecr@hotmail.com>
2017-08-30 18:53:18 -0600

    Presentación

---
+++ PresentationLayer/MainForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            Thread t = new Thread(new ThreadStart(splash));
+            t.Start();
+            Thread.Sleep(15625);
+
+            InitializeComponent();
+
+            t.Abort();
+        }
+
+        public void splash()
+        {
+            Application.Run(new SplashScreen());
+        }
+
+    }
+}
---
+++ PresentationLayer/SplashScreen.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PresentationLayer
+{
+    public partial class SplashScreen : Form
+    {
+        public SplashScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void timerSplashScreen_Tick(object sender, EventArgs e)
+        {
+            prgBarSplash.Increment(1);
+            if (prgBarSplash.Value == 100)
+            {
+                timerSplashScreen.Stop();
+            }
+        }
+    }
+}
